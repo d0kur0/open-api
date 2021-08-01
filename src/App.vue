340c182abf46364a7cfab0dd2030bc42e932617c
d0kur0 <dev.dokuro@gmail.com>
2021-08-01 19:18:21 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="open-api">Open Api component</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style lang="scss">
+.open-api {
+  border: 3px dashed rgb(119, 178, 255);
+  border-radius: 10px;
+  color: rgb(36, 131, 255);
+  background-color: rgb(228, 247, 255);
+  height: 450px;
+  font-size: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+}
+</style>
